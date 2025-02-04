dbe4ff306a6d3353822a2b13254b5e84401e44c5
jay.do <jay.do@nexcel.com>
2025-02-04 16:25:23 +0700

    feat

--- post-shared-test/src/main.jsx
+++ post-shared-test/src/main.jsx
@@ -2,18 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Callback from './Callback.jsx';
 import React from 'react';
-import TwitterCallback from './Callback.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/auth/callback",
-    element: <TwitterCallback />,
   },
 ]);
 
